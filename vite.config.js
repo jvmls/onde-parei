9dd685e3430c4e0328463404857778c875a8bbd7
jvmls <jvmls@cesar.school>
2026-02-10 19:55:29 -0300

    Quick fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/onde-parei/",  // <--- AQUI É O LUGAR CERTO (Na raiz)
   plugins: [
     react({
-      base: "/onde-parei/",
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
